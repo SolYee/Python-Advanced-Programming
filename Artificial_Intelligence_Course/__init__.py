7d003bb9b7ba353918022f6613578a12f217abf8
易首全 <15989434843@163.com>
2023-04-23 10:33:18 +0800

    Initial commit

---
+++ Artificial_Intelligence_Course/__init__.py
@@ -0,0 +1,9 @@
+"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time:2023/4/23 9:46
+# @Author:YiShouquan
+# @File:__init__.py.py
+# @Update:
+"""
+
---
+++ python_Practice.py
@@ -0,0 +1,9 @@
+"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time:2022/9/22 14:11
+# @Author:YiShouquan
+# @File:python_Practice.py
+# @Update:
+"""
+
